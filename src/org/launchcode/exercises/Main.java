01d788fbc25374051b094b8164c7b558d7cec57b
adrianadarris <adrianadarris@gmail.com>
2022-11-11 21:32:08 -0600

    exercises class 9

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -3,7 +3,7 @@ package org.launchcode.exercises;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Main {
+public class Main{
 
     public static void main(String[] args) {
 
@@ -15,17 +15,33 @@ public class Main {
         studentFiles.put("Elizabeth", "MyCode.java");
         studentFiles.put("Stefanie", "CoolProgram.java");
 
+
         // Test out your CheckFileExtension() function!
     }
-
-    public static void Divide(int x, int y)
-    {
+    public static int Divide(int x, int y){
         // Write code here!
+        if (y == 0) {
+            try {
+                throw new ArithmeticException("Whoop, can't do 0");
+            } catch (ArithmeticException a) {
+                a.printStackTrace();
+            }
+        }
+        return x/y;
     }
 
     public static int CheckFileExtension(String fileName)
     {
         // Write code here!
+        int totalPoints = 0;
+        if (fileName.endsWith(".java")){
+            totalPoints += 1;
+        } else if (fileName == null){
+            totalPoints = -1;
+        } else {
+            totalPoints = 0;
+        }
+        return totalPoints;
     }
 
 }
